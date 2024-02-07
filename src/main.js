4dd2880c5b5d7ecdbc2d389dca2f4305bda27fdf
karolina taran <karolina.taran@gmail.com>
2024-02-07 09:28:14 +0100

    add css files

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/modal-menu';
+import './js/quote-of-the-day';
+import './js/exercises-muscules';
+import './js/exercises-body-parts';
+import './js/exercises-equipment';
+import './js/exercises-modal';
+import './js/footer';
