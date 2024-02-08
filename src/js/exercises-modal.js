2f6743cff0667c85a1319121881d79a7e6466be9
MolchanovaOlga <designer.molchanova@gmail.com>
2024-02-08 18:10:54 +0200

    Added open-close of exercises-modal

---
+++ src/js/exercises-modal.js
@@ -0,0 +1,29 @@
+const openModalBtn = document.querySelector('[data-modal-open]');
+const closeModalBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
+
+function modalOpenClose() {
+  openModalBtn.addEventListener('click', toggleModal);
+  closeModalBtn.addEventListener('click', toggleModal);
+  
+  modal.addEventListener('click', event => {
+    if (!event.target.classList.contains("backdrop-exercises-modal")) {
+      return;
+    }
+    toggleModal();
+  });
+
+  document.addEventListener('keydown', closeByEsc);
+  
+  function closeByEsc(event) {
+    if (event.key === "Escape") {
+      modal.classList.remove('is-open');
+    }
+  } 
+
+  function toggleModal() {
+    modal.classList.toggle('is-open');
+  }
+}
+
+modalOpenClose();
