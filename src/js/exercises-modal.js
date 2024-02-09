19596be493846edb119257d2b41b0b61f705b356
Yuriy Dobryanskiy <dobryansky@i.ua>
2024-02-09 02:09:27 +0200

    add blocks

--- src/js/exercises-modal.js
+++ src/js/exercises-modal.js
@@ -5,21 +5,21 @@ const modal = document.querySelector('[data-modal]');
 function modalOpenClose() {
   openModalBtn.addEventListener('click', toggleModal);
   closeModalBtn.addEventListener('click', toggleModal);
-  
+
   modal.addEventListener('click', event => {
-    if (!event.target.classList.contains("backdrop-exercises-modal")) {
+    if (!event.target.classList.contains('backdrop-exercises-modal')) {
       return;
     }
     toggleModal();
   });
 
   document.addEventListener('keydown', closeByEsc);
-  
+
   function closeByEsc(event) {
-    if (event.key === "Escape") {
+    if (event.key === 'Escape') {
       modal.classList.remove('is-open');
     }
-  } 
+  }
 
   function toggleModal() {
     modal.classList.toggle('is-open');
