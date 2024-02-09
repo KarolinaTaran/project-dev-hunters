5107adece7b78e76e8f1a1da4b5445aa11d27787
maxdemo1 <maxbrownwot@gmail.com>
2024-02-09 23:36:13 +0200

    Update exercises-equipment.js

--- src/js/exercises-equipment.js
+++ src/js/exercises-equipment.js
@@ -85,7 +85,7 @@ function resultSearchMakrUp({ results }) {
       <div class="info-about-exercise">
         <div class="exercise-name">
             <svg class="runnig-svg" width="24" height="24">
-                <use href="./img/sprite.svg#running-man"></use>
+                <use href="/project-dev-hunters/assets/sprite-f8222074.svg#running-man"></use>
             </svg>
             <h2>${name[0].toUpperCase() + name.slice(1)}</h2>
         </div>
