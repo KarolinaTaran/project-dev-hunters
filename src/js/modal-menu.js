7a6833957aef5bf4549c81b635fb0f592d2c8d64
ViktoriiaOlex <slavika86@gmail.com>
2024-02-11 13:11:04 +0200

    add js for current pages

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -26,14 +26,46 @@
 //     }
 //   })();
 
+// ------------Variant 2---------------
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const headerMenuToggle = document.querySelector('.button-header');
+//   const menuModal = document.querySelector('.backdrop-menu-modal');
+//   const menuModalCloseButton = document.querySelector('.menu-modal-close-button');
+
+//   // Відкриття модального вікна
+//   headerMenuToggle.addEventListener('click', function() {
+//     menuModal.classList.remove('is-hidden');
+//   });
+
+//   // Закриття модального вікна
+//   menuModalCloseButton.addEventListener('click', function() {
+//     menuModal.classList.add('is-hidden');
+//   });
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
   const headerMenuToggle = document.querySelector('.button-header');
   const menuModal = document.querySelector('.backdrop-menu-modal');
   const menuModalCloseButton = document.querySelector('.menu-modal-close-button');
+  const homeButton = document.querySelector('.modal-menu-button-home');
+  const favoritesButton = document.querySelector('.modal-menu-button-favorites');
+
+  // Визначення поточної сторінки
+  const currentPage = window.location.pathname;
 
   // Відкриття модального вікна
   headerMenuToggle.addEventListener('click', function() {
     menuModal.classList.remove('is-hidden');
+
+    // Зміна стилів кнопок на рожевий background в залежності від сторінки
+    if (currentPage === '/index.html') {
+      homeButton.style.backgroundColor = 'pink';
+      favoritesButton.style.backgroundColor = '';
+    } else if (currentPage === '/favorites.html') {
+      favoritesButton.style.backgroundColor = 'pink';
+      homeButton.style.backgroundColor = '';
+    }
   });
 
   // Закриття модального вікна
