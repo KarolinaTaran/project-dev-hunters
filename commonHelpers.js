66976a2fa689815c2dc25d1a1f071174e6a48e47
KarolinaTaran <139314432+KarolinaTaran@users.noreply.github.com>
2024-02-12 17:32:29 +0000

    Deploying to gh-pages from @ KarolinaTaran/project-dev-hunters@b749cb59ea890bffe5877b83ebbd9c9273506cae 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,35 @@
-import"./assets/arrow-button-39fffa09.js";import{a as u}from"./assets/vendor-8cce9181.js";u.defaults.baseURL="https://energyflow.b.goit.study/api/";const g={quoteText:document.querySelector(".js-quote-text"),quoteAuthor:document.querySelector(".js-quote-author")},{quoteText:c,quoteAuthor:i}=g,l="info",t=JSON.parse(localStorage.getItem(l)),E=new Date().getDate();t===null||E!==t.date?r("quote"):(c.textContent=t.quote,i.textContent=t.author);async function r(a){try{const e=await u.get(`${a}`),{data:{author:f,quote:S}}=e,o={author:f,quote:S,date:E};localStorage.setItem(l,JSON.stringify(o)),c.textContent=o.quote,i.textContent=o.author}catch(e){console.log(e.message)}}const d=document.querySelector(".js-favor-content"),m="exerciseItems",s=JSON.parse(localStorage.getItem(m)),v='<div class="favor-plug-wrap"><div class="favor-icon-wrap"><img class="favor-icon-item" src="./img/dumbbell/dumbbell-desktop.png" alt=""></div><div class="favor-text">It appears that you haven&#8216t added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future</div></div>',p="<ul><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li> </ul>";s===null||(s.length=0)?n():q();function q(a){d.innerHTML=p}function n(){d.innerHTML=v}
+import"./assets/arrow-button-39fffa09.js";import{a as v}from"./assets/vendor-8cce9181.js";v.defaults.baseURL="https://energyflow.b.goit.study/api/";const x={quoteText:document.querySelector(".js-quote-text"),quoteAuthor:document.querySelector(".js-quote-author")},{quoteText:p,quoteAuthor:f}=x,i=document.createElement("ul");i.classList.add("search-result-list");const m="info",n=JSON.parse(localStorage.getItem(m)),h=new Date().getDate();n===null||h!==n.date?g("quote"):(p.textContent=n.quote,f.textContent=n.author);async function g(s){try{const t=await v.get(`${s}`),{data:{author:e,quote:o}}=t,a={author:e,quote:o,date:h};localStorage.setItem(m,JSON.stringify(a)),p.textContent=a.quote,f.textContent=a.author}catch(t){console.log(t.message)}}const c=document.querySelector(".js-favor-content"),u="exerciseItems",r=JSON.parse(localStorage.getItem(u)),b='<div class="favor-plug-wrap"><div class="favor-icon-wrap"><img class="favor-icon-item" src="./img/dumbbell/dumbbell-desktop.png" alt=""></div><div class="favor-text">It appears that you haven&#8216t added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future</div></div>';function l(){c.innerHTML=b}r===null||r!==null&&r.length===0?l():r.length>0&&(S(r),y());function S(s){i.innerHTML=s.map(({bodyPart:t,name:e,target:o,burnedCalories:a,time:q,_id:d})=>`<li class=exercises-serch-result>
+      <div class="rating-start-container">
+        <div class="rating">
+            <p class="workout">Workout</p>
+            <div class="rating-cont">
+              <button type="button" data-id=${d} data-delete-from-favorites>
+                <svg class="icon-trash-svg" width="16" height="16">
+                    <use href="./img/sprite.svg#icon-trash"></use>
+                </svg>
+              </button>
+            </div>
+        </div>
+        <div class="start-button-container">
+            <button type="button" data-id=${d} data-exercise-modal-open>Start
+                <svg class="start-svg" width="18" height="18">
+                    <use href="./img/sprite.svg#icon-arrow-right"></use>
+                </svg>
+            </button>
+        </div>
+      </div>
+      <div class="info-about-exercise">
+        <div class="exercise-name">
+            <svg class="runnig-svg" width="24" height="24">
+                <use href="./img/sprite.svg#running-man"></use>
+            </svg>
+            <h2>${e[0].toUpperCase()+e.slice(1)}</h2>
+        </div>
+        <div class="additional-information">
+            <p class=>Burned calories: <span class="info-from-back">${a} / ${q} min</span></p>
+            <p class=>Body part: <span class="info-from-back">${t[0].toUpperCase()+t.slice(1)}</span></p>
+            <p class=>Target: <span class="info-from-back">${o[0].toUpperCase()+o.slice(1)}</span></p>
+        </div>
+     
+          </li>`).join(""),c.innerHTML="",c.prepend(i)}function y(){document.querySelectorAll("[data-delete-from-favorites]").forEach(t=>{t.addEventListener("click",L)})}function L(s){const t=s.currentTarget.dataset.id,e=JSON.parse(localStorage.getItem(u)).filter(({_id:o})=>{if(o!==t)return!0});if(localStorage.setItem(u,JSON.stringify(e)),e.length===0){l();return}S(e),y()}
 //# sourceMappingURL=commonHelpers.js.map
