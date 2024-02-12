f985b889ae2fe9218c7087ae24c761710de37391
KarolinaTaran <139314432+KarolinaTaran@users.noreply.github.com>
2024-02-12 19:42:20 +0000

    Deploying to gh-pages from @ KarolinaTaran/project-dev-hunters@8d9a23669f24a650e458aed40578ed963fa11a2d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,9 @@
-import"./assets/arrow-button-39fffa09.js";import{a as v}from"./assets/vendor-8cce9181.js";v.defaults.baseURL="https://energyflow.b.goit.study/api/";const x={quoteText:document.querySelector(".js-quote-text"),quoteAuthor:document.querySelector(".js-quote-author")},{quoteText:p,quoteAuthor:f}=x,i=document.createElement("ul");i.classList.add("search-result-list");const m="info",n=JSON.parse(localStorage.getItem(m)),h=new Date().getDate();n===null||h!==n.date?g("quote"):(p.textContent=n.quote,f.textContent=n.author);async function g(s){try{const t=await v.get(`${s}`),{data:{author:e,quote:o}}=t,a={author:e,quote:o,date:h};localStorage.setItem(m,JSON.stringify(a)),p.textContent=a.quote,f.textContent=a.author}catch(t){console.log(t.message)}}const c=document.querySelector(".js-favor-content"),u="exerciseItems",r=JSON.parse(localStorage.getItem(u)),b='<div class="favor-plug-wrap"><div class="favor-icon-wrap"><img class="favor-icon-item" src="./img/dumbbell/dumbbell-desktop.png" alt=""></div><div class="favor-text">It appears that you haven&#8216t added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future</div></div>';function l(){c.innerHTML=b}r===null||r!==null&&r.length===0?l():r.length>0&&(S(r),y());function S(s){i.innerHTML=s.map(({bodyPart:t,name:e,target:o,burnedCalories:a,time:q,_id:d})=>`<li class=exercises-serch-result>
+import{c as v}from"./assets/exercises-modal-adbd807a.js";import{a as p}from"./assets/vendor-8cce9181.js";p.defaults.baseURL="https://energyflow.b.goit.study/api/";const b={quoteText:document.querySelector(".js-quote-text"),quoteAuthor:document.querySelector(".js-quote-author")},{quoteText:m,quoteAuthor:h}=b,t=document.createElement("ul");t.classList.add("search-result-list");t.id="scrollTry";const S="info",i=JSON.parse(localStorage.getItem(S)),y=new Date().getDate();i===null||y!==i.date?f("quote"):(m.textContent=i.quote,h.textContent=i.author);async function f(o){try{const e=await p.get(`${o}`),{data:{author:s,quote:r}}=e,a={author:s,quote:r,date:y};localStorage.setItem(S,JSON.stringify(a)),m.textContent=a.quote,h.textContent=a.author}catch(e){console.log(e.message)}}const c=document.querySelector(".js-favor-content"),l="exerciseItems",n=JSON.parse(localStorage.getItem(l)),k='<div class="favor-plug-wrap"><div class="favor-icon-wrap"><img class="favor-icon-item" src="./img/dumbbell/dumbbell-desktop.png" alt=""></div><div class="favor-text">It appears that you haven&#8216t added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future</div></div>';function d(){c.innerHTML=k}n===null||n!==null&&n.length===0?d():n.length>0&&(q(n),w(),v(),x());function q(o){t.innerHTML=o.map(({bodyPart:e,name:s,target:r,burnedCalories:a,time:L,_id:u})=>`<li class=exercises-serch-result>
       <div class="rating-start-container">
         <div class="rating">
             <p class="workout">Workout</p>
             <div class="rating-cont">
-              <button type="button" data-id=${d} data-delete-from-favorites>
+              <button type="button" data-id=${u} data-delete-from-favorites>
                 <svg class="icon-trash-svg" width="16" height="16">
                     <use href="./img/sprite.svg#icon-trash"></use>
                 </svg>
@@ -11,7 +11,7 @@ import"./assets/arrow-button-39fffa09.js";import{a as v}from"./assets/vendor-8cc
             </div>
         </div>
         <div class="start-button-container">
-            <button type="button" data-id=${d} data-exercise-modal-open>Start
+            <button type="button" data-id=${u} data-exercise-modal-open>Start
                 <svg class="start-svg" width="18" height="18">
                     <use href="./img/sprite.svg#icon-arrow-right"></use>
                 </svg>
@@ -23,13 +23,13 @@ import"./assets/arrow-button-39fffa09.js";import{a as v}from"./assets/vendor-8cc
             <svg class="runnig-svg" width="24" height="24">
                 <use href="./img/sprite.svg#running-man"></use>
             </svg>
-            <h2>${e[0].toUpperCase()+e.slice(1)}</h2>
+            <h2>${s[0].toUpperCase()+s.slice(1)}</h2>
         </div>
         <div class="additional-information">
-            <p class=>Burned calories: <span class="info-from-back">${a} / ${q} min</span></p>
-            <p class=>Body part: <span class="info-from-back">${t[0].toUpperCase()+t.slice(1)}</span></p>
-            <p class=>Target: <span class="info-from-back">${o[0].toUpperCase()+o.slice(1)}</span></p>
+            <p class=>Burned calories: <span class="info-from-back">${a} / ${L} min</span></p>
+            <p class=>Body part: <span class="info-from-back">${e[0].toUpperCase()+e.slice(1)}</span></p>
+            <p class=>Target: <span class="info-from-back">${r[0].toUpperCase()+r.slice(1)}</span></p>
         </div>
      
-          </li>`).join(""),c.innerHTML="",c.prepend(i)}function y(){document.querySelectorAll("[data-delete-from-favorites]").forEach(t=>{t.addEventListener("click",L)})}function L(s){const t=s.currentTarget.dataset.id,e=JSON.parse(localStorage.getItem(u)).filter(({_id:o})=>{if(o!==t)return!0});if(localStorage.setItem(u,JSON.stringify(e)),e.length===0){l();return}S(e),y()}
+          </li>`).join(""),c.innerHTML="",c.prepend(t)}function w(){document.querySelectorAll("[data-delete-from-favorites]").forEach(e=>{e.addEventListener("click",T)})}function T(o){const e=o.currentTarget.dataset.id,s=JSON.parse(localStorage.getItem(l)).filter(({_id:r})=>{if(r!==e)return!0});if(localStorage.setItem(l,JSON.stringify(s)),s.length===0){d();return}q(s),w(),v(),x()}function x(){window.innerWidth>=768&&window.innerWidth<1440&&t.children.length>8||window.innerWidth>=1440&&t.children.length>9?g():I()}function g(){t.classList.add("scroll-on"),t.classList.add("padding-for-scroll-list"),document.querySelector(".favor-wrapper").classList.add("padding-for-scroll-container")}function I(){t.classList.remove("scroll-on"),t.classList.remove("padding-for-scroll-list"),document.querySelector(".favor-wrapper").classList.remove("padding-for-scroll-container")}
 //# sourceMappingURL=commonHelpers.js.map
