76c392b5b45744f6de4e721e91f17ca33e5cc91d
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-02-12 17:38:40 +0200

    add markup container and template, section favorites, page favorites

--- src/favorites.js
+++ src/favorites.js
@@ -56,3 +56,53 @@ async function requestQuote(endPoint) {
     console.log(error.message);
   }
 }
+
+// -----------SECTION FAVORITES------------
+
+const favorContent = document.querySelector('.js-favor-content');
+
+// Получение данных из LS
+const FAVORITES_LS_KEY = 'exerciseItems';
+const favorExercLS = JSON.parse(localStorage.getItem(FAVORITES_LS_KEY));
+
+// Разметка для заглушки
+const markupPlug =
+  '<div class="favor-plug-wrap"><div class="favor-icon-wrap"><img class="favor-icon-item" src="./img/dumbbell/dumbbell-desktop.png" alt=""></div><div class="favor-text">It appears that you haven&#8216t added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future</div></div>';
+
+// Шаблон разметка для карточек с упраженениями
+const markupCard =
+  '<ul><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li><li>FAVORITE EXERCISES</li> </ul>';
+
+// Прверкa на null (если нет данных в LS с таким ключом) и на длину массива
+if (favorExercLS === null) {
+  createMarkupNonFavExers();
+} else if ((favorExercLS.length = 0)) {
+  createMarkupNonFavExers();
+} else {
+  createMarkupFavExers(favorExercLS);
+}
+
+// Функция для разметки из массива объектов упражнений
+function createMarkupFavExers(arr) {
+  favorContent.innerHTML = markupCard;
+}
+
+// Функция для разметки, если нет упраженений, выводит сообщение
+function createMarkupNonFavExers() {
+  favorContent.innerHTML = markupPlug;
+}
+
+// Объект с информацией по упражнению
+// {
+//   "_id": "64f389465ae26083f39b17a2",
+//   "bodyPart": "waist",
+//   "equipment": "body weight",
+//   "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0001.gif",
+//   "name": "3/4 sit-up",
+//   "target": "abs",
+//   "description": "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+//   "rating": 3.49,
+//   "burnedCalories": 220,
+//   "time": 3,
+//   "popularity": 9953
+// }
