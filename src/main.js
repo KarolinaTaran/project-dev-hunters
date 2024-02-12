8aaefb17dd778d48a3b9cad8e29d5f7bb4041087
karolina taran <karolina.taran@gmail.com>
2024-02-12 16:47:25 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './js/modal-menu';
 import './js/quote-of-the-day';
-import './js/exercises-muscules';
 import './js/exercises-body-parts';
 import './js/exercises-equipment';
 import './js/exercises-modal';
