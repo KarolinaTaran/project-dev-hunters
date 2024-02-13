357be98c2c3757cd4fe446ead9d30bc2bc5a70fd
maxdemo1 <maxbrownwot@gmail.com>
2024-02-13 20:11:33 +0200

    Update modal-menu.js

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -1,20 +1,23 @@
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const headerMenuToggle = document.querySelector('.button-header');
   const menuModal = document.querySelector('.backdrop-menu-modal');
-  const menuModalCloseButton = document.querySelector('.menu-modal-close-button');
+  const menuModalCloseButton = document.querySelector(
+    '.menu-modal-close-button'
+  );
   const homeButton = document.querySelector('.modal-menu-button-home');
-  const favoritesButton = document.querySelector('.modal-menu-button-favorites');
+  const favoritesButton = document.querySelector(
+    '.modal-menu-button-favorites'
+  );
   const headerLogo = document.querySelector('.header-logo');
 
   // Визначення поточної сторінки
   const currentPage = window.location.pathname;
 
   // Відкриття модального вікна
-  headerMenuToggle.addEventListener('click', function() {
+  headerMenuToggle.addEventListener('click', function () {
     menuModal.classList.remove('is-hidden');
 
-    document.body.style.position = "fixed";
+    document.body.style.position = 'fixed';
     headerLogo.style.display = 'none';
 
     // Зміна стилів кнопок на рожевий background в залежності від сторінки
@@ -34,8 +37,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Закриття модального вікна
-  menuModalCloseButton.addEventListener('click', function() {
+  menuModalCloseButton.addEventListener('click', function () {
     menuModal.classList.add('is-hidden');
     headerLogo.style.display = '';
+    document.body.style.position = 'relative';
   });
 });
