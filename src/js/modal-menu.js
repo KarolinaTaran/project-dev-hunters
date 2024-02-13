5311b61700c235a6f814db580625a5da10e6aff9
ViktoriiaOlex <slavika86@gmail.com>
2024-02-13 14:06:08 +0200

    add styles to buttons in js

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -9,28 +9,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Визначення поточної сторінки
   const currentPage = window.location.pathname;
-  console.log(currentPage);
 
   // Відкриття модального вікна
   headerMenuToggle.addEventListener('click', function() {
     menuModal.classList.remove('is-hidden');
 
-    // document.body.classList.toggle('no-scroll');
-    // document.body.classList.add('position: fixed');
     document.body.style.position = "fixed";
-
-   
     headerLogo.style.display = 'none';
 
     // Зміна стилів кнопок на рожевий background в залежності від сторінки
     if (currentPage === '/project-dev-hunters/index.html') {
-      homeButton.style.backgroundColor = 'pink';
+      homeButton.style.backgroundColor = 'var(--primary-white-color)';
+      homeButton.style.color = 'var( --main-black-color)';
+
       favoritesButton.style.backgroundColor = '';
+      favoritesButton.style.color = '';
     } else if (currentPage === '/project-dev-hunters/favorites.html') {
-      favoritesButton.style.backgroundColor = 'pink';
+      favoritesButton.style.backgroundColor = 'var(--primary-white-color)';
+      favoritesButton.style.color = ' var( --main-black-color)';
+
       homeButton.style.backgroundColor = '';
+      homeButton.style.color = '';
     }
-  
   });
 
   // Закриття модального вікна
